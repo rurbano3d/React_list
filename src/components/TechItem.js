8a8827a7d42d585be1724c60d665319f8d272554
rurbano3d <rurbano3d@gmail.com>
2019-09-04 08:45:42 -0300

    state and component

---
+++ src/components/TechItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TechItem ({tech, onDelete}){
+  return (
+    <li>
+      {tech}
+      <button type="button" onClick={onDelete}>Remover</button>
+    </li>
+  );
+}
+
+TechItem.propTypes = {
+  tech: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired
+}
+
+export default TechItem;
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,20 +1,68 @@
 import React,{ Component } from 'react';
 
+import TechItem from './TechItem'
+
 class TechList extends Component{
   state = {
-    techs:[
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    newTech:'',
+    techs:[]
   };
+  //Component lifecycle
+
+  //Execute when component render
+  componentDidMount(){
+    const techs = localStorage.getItem('techs');
+
+    if(techs){
+      this.setState({techs:JSON.parse(techs)});
+    }
+  }
+
+  //Execute always you have a change
+  componentDidUpdate(prevProps, prevState){
+    //this.props, this.state
+    if (prevState.techs !== this.state.techs){
+      localStorage.setItem('techs',JSON.stringify(this.state.techs));
+    }
+  }
+
+  //Execute when destroy component
+  componentWillUnmount(){
+
+  }
+
+  //get input value.To use this, i need to create arrow function inside of class. 
+  handleInputChange = e =>{
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+    this.setState({
+      techs:[...this.state.techs,this.state.newTech],
+      newTech:''
+    });
+  }
+
+  //filter return all techs (t) be different of tech, ex: tech is Node.js, t return reactJS and react native cause this techs is different of tech state sent
+  handleDelete = (tech) =>{
+    this.setState({ techs: this.state.techs.filter( t => t !== tech) })
+  }
+
   render(){
     return(
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (<TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)}/>))}
+          
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={this.state.newTech}/>
+          <button type="submit">Enviar</button>
+      </form>
+      
     );
   }
 }
